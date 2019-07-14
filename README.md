5d7f1e46f176b6be0cabf6c6466ea57aedf0bf98
Steven Robichaud <srrobichaud92@gmail.com>
2019-07-14 19:43:50 -0400

    Initial commit, full human playable snake, starting AI snake, updated README and gitignore

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 tf_py_snake
+
+The game snake, made with pygame.
+Learning tensorflow, get it to play the game.
+
+├── README.md         # This file
+├── snake_ai
+│   └── snake_ai.py   # AI version of game, has computer guess which way to go
+├── snake_game
+│   └── snake.py      # Human playable version of snake
+
+Dependancies:
+
+	python3
+	pygame
+	tensorflow-2.0.0-beta1
+
+Human playable:
+	Just hit a direction key and you're off, continue playing until you hit a wall or yourself!
+	Once a lose is detected ESCAPE or X button will exit, SPACE will start new game
+
---
+++ snake_ai/snake_ai.py
@@ -0,0 +1,175 @@
+import sys
+import pygame
+import random
+
+# Player Class
+# Handles how big the player is, the direction the player is moving
+
+
+class Player:
+  speed = 10
+
+  def __init__(self, x=200, y=200):
+    self.x = x
+    self.y = y
+
+  def moveRight(self):
+    self.x = self.x + self.speed
+
+  def moveLeft(self):
+    self.x = self.x - self.speed
+
+  def moveUp(self):
+    self.y = self.y - self.speed
+
+  def moveDown(self):
+    self.y = self.y + self.speed
+
+  def reset(self):
+    self.x = 200
+    self.y = 200
+
+
+class Food:
+  x = 0
+  y = 0
+  size = 10
+
+  def __init__(self):
+    self.rand_pos()
+
+  def rand_pos(self):
+    random.seed()
+    self.x = random.randrange(0, 400 - self.size, self.size)
+    self.y = random.randrange(0, 400 - self.size, self.size)
+
+
+class Game:
+  screenWidth = 400                    # Screen Width
+  screenHeight = 400                   # Screen Height
+  size = screenWidth, screenHeight     # Array of the screen size
+  black = 0, 0, 0                      # The color settings for black
+  white = 255, 255, 255                # The color settings for white
+  red = 255, 0, 0                      # The color settings for red
+  snake = []                           # Instance of the player class
+  gameExit = False                     # Game state, Exit/Quit state
+  gamePause = False                    # Game state, Pause state
+  gameRestart = False                  # Game state, Restart state
+  curDir = False, False, False, False  # Right, Left, Up, Down
+  clock = pygame.time.Clock()          # Clock for FPS
+  fps = 10                             # FPS
+  block_size = 10                      # Size of snake in pixels
+  food = Food()                        # Food piece
+
+  def __init__(self):
+    pygame.init()
+    self.screen = pygame.display.set_mode(self.size)
+    pygame.display.set_caption('Snake')
+    self.snake.append(Player())
+
+  def run(self):
+    while not self.gameExit:
+
+      # Create "user" input
+      random.seed()
+      ai_input = random.randrange(0, 4, 1)
+      if ai_input == 0:
+        key_event = pygame.event.Event(pygame.KEYDOWN, key=pygame.K_RIGHT)
+        pygame.event.post(key_event)
+      elif ai_input == 1:
+        key_event = pygame.event.Event(pygame.KEYDOWN, key=pygame.K_LEFT)
+        pygame.event.post(key_event)
+      elif ai_input == 2:
+        key_event = pygame.event.Event(pygame.KEYDOWN, key=pygame.K_DOWN)
+        pygame.event.post(key_event)
+      else:
+        key_event = pygame.event.Event(pygame.KEYDOWN, key=pygame.K_UP)
+        pygame.event.post(key_event)
+
+      for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+          if event.key == pygame.K_RIGHT and not(self.curDir[1]):
+            self.curDir = True, False, False, False
+          elif event.key == pygame.K_LEFT and not(self.curDir[0]):
+            self.curDir = False, True, False, False
+          elif event.key == pygame.K_DOWN and not(self.curDir[3]):
+            self.curDir = False, False, True, False
+          elif event.key == pygame.K_UP and not(self.curDir[2]):
+            self.curDir = False, False, False, True
+
+      # Update snake pos
+      self.update_pos()
+
+      # Check if food is overlapping
+      if self.snake[0].x == self.food.x and self.snake[0].y == self.food.y:
+        self.snake.append(Player(self.snake[-1].x, self.snake[-1].y))
+        self.food.rand_pos()
+      # Detect edges of screen
+      elif self.snake[0].x < 0 or self.snake[0].x > self.screenWidth - self.block_size or self.snake[0].y < 0 or self.snake[0].y > self.screenHeight - self.block_size or self.check_overlap():
+        self.restart()
+
+        # Update screen
+      self.screen.fill(self.black)
+      pygame.draw.rect(self.screen, self.red, [
+                       self.food.x, self.food.y, self.block_size, self.block_size])
+      self.draw_snake()
+      pygame.display.update()
+      self.clock.tick(self.fps)
+
+    pygame.quit()
+    sys.exit()
+
+  def restart(self):
+    while not self.gameRestart and not self.gameExit:
+      # Get user input
+      for event in pygame.event.get():
+        if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+          self.gameExit = True
+        elif event.type == pygame.KEYDOWN:
+          if event.key == pygame.K_SPACE:
+            self.gameRestart = True
+    if self.gameRestart:
+      self.gameRestart = False
+      self.curDir = False, False, False, False
+      self.snake = []
+      self.snake.append(Player())
+      self.run()
+    else:
+      pygame.quit()
+      sys.exit()
+
+  def update_pos(self):
+    for i, block in reversed(list(enumerate(self.snake))):
+      if i == 0:
+        # Act on user input
+        if self.curDir[0]:
+          block.moveRight()
+        elif self.curDir[1]:
+          block.moveLeft()
+        elif self.curDir[2]:
+          block.moveDown()
+        elif self.curDir[3]:
+          block.moveUp()
+      else:
+        block.x = self.snake[i - 1].x
+        block.y = self.snake[i - 1].y
+
+  def draw_snake(self):
+    for block in self.snake:
+      pygame.draw.rect(self.screen, self.white, [
+          block.x, block.y, self.block_size, self.block_size])
+
+  def check_overlap(self):
+    for block in self.snake[1:]:
+      if block.x == self.snake[0].x and block.y == self.snake[0].y:
+        return True
+    return False
+
+
+if __name__ == "__main__":
+  snake_game = Game()
+  snake_game.run()
+
+
+def main():
+  pass
