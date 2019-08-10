d526edd31611e3147962f64e8330cbc16585890b
Steven Robichaud <srrobichaud92@gmail.com>
2019-08-10 08:20:17 -0400

    Remaned ai folder to nn_snake_ai since thinking of trying other ML methods. Also made some slight changes to human playable version

--- snake_game/snake.py
+++ snake_game/snake.py
@@ -2,6 +2,8 @@ import sys
 import pygame
 import random
 
+start_size = 5  # How large to start snake
+
 # Player Class
 # Handles how big the player is, the direction the player is moving
 
@@ -45,30 +47,40 @@ class Food:
 
 
 class Game:
-  screenWidth = 400										 # Screen Width
-  screenHeight = 400 									 # Screen Height
-  size = screenWidth, screenHeight 		 # Array of the screen size
-  black = 0, 0, 0	 										 # The color settings for black
-  white = 255, 255, 255 							 # The color settings for white
-  red = 255, 0, 0	 										 # The color settings for red
-  snake = []									         # Instance of the player class
-  gameExit = False 										 # Game state, Exit/Quit state
+  screenWidth = 400                    # Screen Width
+  screenHeight = 400                   # Screen Height
+  size = screenWidth, screenHeight     # Array of the screen size
+  black = 0, 0, 0                      # The color settings for black
+  white = 255, 255, 255                # The color settings for white
+  red = 255, 0, 0                      # The color settings for red
+  snake = []                           # Instance of the player class
+  gameExit = False                     # Game state, Exit/Quit state
   gamePause = False                    # Game state, Pause state
   gameRestart = False                  # Game state, Restart state
-  curDir = False, False, False, False  # Right, Left, Up, Down
+  curDir = True, False, False, False  # Right, Left, Up, Down
   clock = pygame.time.Clock()          # Clock for FPS
   fps = 10                             # FPS
-  block_size = 10	 										 # Size of snake in pixels
-  food = Food() 											 # Food piece
+  score = 0                            # Score to display
+  block_size = 10                      # Size of snake in pixels
+  food = Food()                        # Food piece
+  global start_size
 
   def __init__(self):
     pygame.init()
     self.screen = pygame.display.set_mode(self.size)
-    pygame.display.set_caption('Snake')
     self.snake.append(Player())
+    for _ in range(start_size):
+      self.snake.append(
+          Player(self.snake[-1].x - self.block_size, self.snake[-1].y))
 
   def run(self):
+
+    self.score = 0
+    self.curDir = True, False, False, False
+
     while not self.gameExit:
+
+      pygame.display.set_caption('Snake' + '        Score:' + str(self.score))
 
       # Get user input
       for event in pygame.event.get():
@@ -91,6 +103,7 @@ class Game:
       if self.snake[0].x == self.food.x and self.snake[0].y == self.food.y:
         self.snake.append(Player(self.snake[-1].x, self.snake[-1].y))
         self.food.rand_pos()
+        self.score += 1
       # Detect edges of screen
       elif self.snake[0].x < 0 or self.snake[0].x > self.screenWidth - self.block_size or self.snake[0].y < 0 or self.snake[0].y > self.screenHeight - self.block_size or self.check_overlap():
         self.restart()
@@ -117,9 +130,12 @@ class Game:
             self.gameRestart = True
     if self.gameRestart:
       self.gameRestart = False
-      self.curDir = False, False, False, False
+      self.curDir = True, False, False, False
       self.snake = []
       self.snake.append(Player())
+      for _ in range(start_size):
+        self.snake.append(
+            Player(self.snake[-1].x - self.block_size, self.snake[-1].y))
       self.run()
     else:
       pygame.quit()
@@ -145,6 +161,8 @@ class Game:
     for block in self.snake:
       pygame.draw.rect(self.screen, self.white, [
           block.x, block.y, self.block_size, self.block_size])
+      pygame.draw.rect(self.screen, self.black, [
+          block.x, block.y, self.block_size, self.block_size], 1)
 
   def check_overlap(self):
     for block in self.snake[1:]:
@@ -157,5 +175,6 @@ if __name__ == "__main__":
   snake_game = Game()
   snake_game.run()
 
+
 def main():
-	pass
+  pass
